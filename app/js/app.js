58de77e8b24621d20b21c506a7339c73d4ef49a1
dtrinh888 <dtrinh888@gmail.com>
2016-04-26 21:23:24 -0700

    created fake comment data

--- app/js/app.js
+++ app/js/app.js
@@ -7,46 +7,67 @@ app.factory('postsFactory', [function(){
 	return o;
 }]);
 
-app.controller('MainCtrl', ['$scope', 'postsFactory',
-	function($scope, postsFactory){
-		$scope.posts = postsFactory.posts;
+app.controller('MainCtrl', ['$scope', 'postsFactory', function($scope, postsFactory){
+	$scope.posts = postsFactory.posts;
 
-		$scope.posts = [
-			{title: 'post 1', upvotes: 5},
-			{title: 'post 2', upvotes: 2},
-			{title: 'post 3', upvotes: 15},
-			{title: 'post 4', upvotes: 9},
-			{title: 'post 5', upvotes: 4},
-		];
+	$scope.posts = [
+		{title: 'post 1', upvotes: 5},
+		{title: 'post 2', upvotes: 2},
+		{title: 'post 3', upvotes: 15},
+		{title: 'post 4', upvotes: 9},
+		{title: 'post 5', upvotes: 4},
+	];
 
-		$scope.addPost = function(){
-			if ($scope.title || $scope.title === ''){
-				return;
-			}
+	$scope.addPost = function(){
+		if ($scope.title || $scope.title === ''){
+			return;
+		}
 
-			$scope.posts.push({
-				title: $scope.title,
-				link: $scope.link,
-				upvotes: 0
-			});
-			$scope.title = '';
-			$scope.link = '';
-		};
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link,
+			upvotes: 0, 
+			comments: [
+				{
+					author: 'Joe',
+					body: 'Cool Post',
+					upvotes: 0
+				},
+				{
+					author: 'Bob',
+					body: 'Great idea but everything is wrong!',
+					upvotes: 0
+				}
+			]
+		});
+		$scope.title = '';
+		$scope.link = '';
+	};
 
-		$scope.incrementUpvotes = function(post){
-			post.upvotes += 1;
-		};
+	$scope.incrementUpvotes = function(post){
+		post.upvotes += 1;
+	};
 
-	}]);
+}]);
+
+app.controller('PostsCtrl', ['$scope', '$stateParams', 'postsFactory', function($scope, $stateParams, postsFactory){
+	//scope object that grabs the appropriate post from the postsFactory using the id from $stateParams
+	$scope.post = postsFactory.posts[$stateParams.id];
+}]);
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
 
 	$stateProvider
-		.state('home', {
+		.state('homeRoute', {
 			url: '/home',
 			templateUrl: '/home.html',
 			controller: 'MainCtrl'
-	});
+		})
+		.state('postsRoute', {
+			url: '/posts/{id}',
+			templateUrl: 'posts.html',
+			controller: 'PostsCtrl'
+		});
 
 	$urlRouterProvider.otherwise('home');
 }]);
